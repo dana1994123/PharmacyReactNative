94495d6d864c640c32085e73d21c6bbeefbdf6fb
dana <danaaljamal94@gmail.com>
2021-04-04 12:06:08 -0400

    solve conflicts

--- src/database/config.js
+++ src/database/config.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import * as firebase from 'firebase';
-
-=======
 // import firestore from "@react-native-firebase/firestore";
 
 // const usersCollection = firestore().collection("Users");
 import firebase from '@react-native-firebase/app';
->>>>>>> 61511c087acff7ff226a0e93be84ec54c2624220
 const firebaseConfig = {
   apiKey: 'AIzaSyA7iSdAJjwcJErmhuzSbDCZqkKQZI2fwfQ',
   authDomain: 'pharmacy-app-b935b.firebaseapp.com',
@@ -17,13 +12,8 @@ const firebaseConfig = {
   appId: '1:970418650183:web:9d71a17c17fa7c159dc562',
 };
 
-<<<<<<< HEAD
-const app = firebase.initializeApp(firebaseConfig);
-export const db = app.firebase;
-=======
 //const app = firebase.initializeApp(firebaseConfig);
 export const db = firebase.Firebase;
->>>>>>> 61511c087acff7ff226a0e93be84ec54c2624220
 
 // //storing a document named "mario" inside of a collection named "characters" in Firestore:
 // dbh.collection("characters").doc("mario").set({
