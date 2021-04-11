db7107d96ad5318862299961d14e11e3a409186e
Seth Climenhaga <sethclim@gmail.com>
2021-04-11 11:16:17 -0400

    clean up files after pull

--- src/ui/components/SelectForm.js
+++ src/ui/components/SelectForm.js
@@ -1,52 +1,10 @@
-// import React, {Component} from 'react';
-// import {
-//   Text,
-//   View,
-//   TouchableWithoutFeedback,
-//   Keyboard,
-//   Button,
-// } from 'react-native';
-// import {Picker} from '@react-native-picker/picker';
-// import {forms} from '../../res/styles/global';
-// import R from '../../res/R';
-
-// export default class SelectForm extends React.Component {
-//   render() {
-//     return (
-//       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-//         <View style={{flex: 1}}>
-//           <Picker
-//           itemStyle ={{color:R.colors.orange}}
-//             style={forms.formcon}
-//             selectedValue={this.props.value}
-//             onValueChange={this.props.onFieldChange.bind(
-//               null,
-//               this.props.fieldName,
-//             )}>
-//             {this.props.options.map((item, key) => {
-//               return (
-//                 <Picker.Item label={item.toString()} key={key} value={item} />
-//               );
-//             })}
-//           </Picker>
-//         </View>
-//       </TouchableWithoutFeedback>
-//     );
-//   }
-// }
 import React, {Component} from 'react';
-import {
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Button,
-} from 'react-native';
+import {View, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import {button, forms} from '../../res/styles/global';
-import { AppButton } from './AppButton';
+import {AppButton} from './AppButton';
 
-export default class SelectForm extends React.Component {
+export default class SelectForm extends Component {
   render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -73,7 +31,6 @@ export default class SelectForm extends React.Component {
               buttonStyle={button.Wrap}
               textStyle={button.Text}
             />
-            
           </View>
         </View>
       </TouchableWithoutFeedback>
