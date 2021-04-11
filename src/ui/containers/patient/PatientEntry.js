9baee0b0c26e185ea9a01ff92b587d3a858794f3
Seth Climenhaga <sethclim@gmail.com>
2021-04-11 18:51:34 -0400

    icons for tab navigaters

--- src/ui/containers/patient/PatientEntry.js
+++ src/ui/containers/patient/PatientEntry.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import Profile from './Profile';
-import Home from '../pharmacy/Home';
-import LogIn from '../common/Login';
 import PatientHome from './Home';
-import AddPrescription from './AddPrescription';
 import Support from './Support';
-import FAQuestion from './FAQuestion';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import Clock from './drug_reminder/Clock';
 import R from '../../../res/R.js';
 import News from '../common/news/News';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -18,20 +13,50 @@ export default function PatientEntry() {
   return (
     <Tab.Navigator
       shifting={false}
-      activeColor={R.colors.white}
-      inactiveColor={R.colors.lightGrey}
-      barStyle={{backgroundColor: R.colors.primary}}>
-      <Tab.Screen name="Patient3" options={{title: 'Home'}}>
+      activeColor={R.colors.orange}
+      inactiveColor={R.colors.grey}
+      barStyle={{backgroundColor: R.colors.white}}>
+      <Tab.Screen
+        name="Patient3"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({focused, color, size}) => {
+            return <Ionicons name={'home'} size={20} color={color} />;
+          },
+        }}>
         {() => <PatientHome name="PatientHome" />}
       </Tab.Screen>
-      <Tab.Screen name="Patient1" options={{title: 'Profile'}}>
+      <Tab.Screen
+        name="Patient1"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <Ionicons name={'person-circle-sharp'} size={20} color={color} />
+            );
+          },
+        }}>
         {() => <Profile name="Profile" />}
       </Tab.Screen>
-      <Tab.Screen name="Patient2" options={{title: 'News'}}>
+      <Tab.Screen
+        name="Patient2"
+        options={{
+          title: 'News',
+          tabBarIcon: ({focused, color, size}) => {
+            return <Ionicons name={'newspaper'} size={20} color={color} />;
+          },
+        }}>
         {() => <News name="News" />}
       </Tab.Screen>
 
-      <Tab.Screen name="Patient4" options={{title: 'Support'}}>
+      <Tab.Screen
+        name="Patient4"
+        options={{
+          title: 'Support',
+          tabBarIcon: ({focused, color, size}) => {
+            return <Ionicons name={'call'} size={20} color={color} />;
+          },
+        }}>
         {() => <Support name="SupportPage" />}
       </Tab.Screen>
     </Tab.Navigator>
--- src/ui/containers/pharmacy/Entry.js
+++ src/ui/containers/pharmacy/Entry.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import R from '../../../res/R';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import FillOrderFlow from './FillOrderFlow';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -10,14 +11,32 @@ export default function entry() {
   return (
     <Tab.Navigator
       shifting={false}
-      activeColor={R.colors.black}
-      inactiveColor={R.colors.lightGrey}
-      barStyle={{backgroundColor: R.colors.white}}>
-      <Tab.Screen name="Pharmacy" component={Home} options={{title: 'Home'}} />
+      activeColor={R.colors.white}
+      inactiveColor={R.colors.blueGrey}
+      barStyle={{backgroundColor: R.colors.primary}}>
+      <Tab.Screen
+        name="Pharmacy"
+        component={Home}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = 'home';
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={20} color={color} />;
+          },
+        }}
+      />
       <Tab.Screen
         name="FillOrder"
         component={FillOrderFlow}
-        options={{title: 'Fill Order'}}
+        options={{
+          title: 'Fill Order',
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = 'clipboard';
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={20} color={color} />;
+          },
+        }}
       />
     </Tab.Navigator>
   );
